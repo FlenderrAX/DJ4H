fa747ce81f66be09b3adb88d7962b23d1d841c50
FlenderrAX <74816698+FlenderrAX@users.noreply.github.com>
2026-08-15 13:01:32 +0100

    feat: added RNGdle profile, server stats, and leaderboard UI

--- utils/rngdle.py
+++ utils/rngdle.py
@@ -165,7 +165,6 @@ class Tier(Enum):
     MYTHIC = 7
     ERROR = 8
 
-
 # From dark theme
 # TIER_TO_COLOR = {
 #     Tier.TRASH: (255, 210, 48), # dark
@@ -201,12 +200,20 @@ TIER_TO_COLOR = {
 }
 
 
+<<<<<<< HEAD
+def get_score_tier_from_table(score: int):
+    if score not in SCORE_TO_PERCENT:
+        LOGGER.warning(f"RNGdle: unexpected score {score} (not in table).")
+        # Enable if you prefer to not consider unknown scores
+        # return Tier.ERROR
+=======
 def get_score_tier_from_compressed_table(score: int):
     if score < 0:
         LOGGER.warning(f"RNGdle: unexpected negative score ({score}).")
         return Tier.ERROR
 
     percent = get_score_percent(score)
+>>>>>>> upstream/master
 
     if 0 <= percent < 1:
         tier = Tier.TRASH
